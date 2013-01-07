eadb0f8a3e5c336126b2f15cfe511066c6fec400
songhairui1110@gmail.com <songhairui1110@gmail.com@47e0d3c2-fcb2-9a0f-c3f3-96d527b66d0c>
2013-01-07 04:03:22 +0000

    make dal interface

--- trunk/shop/IDAL/IChangeStock.cs
+++ trunk/shop/IDAL/IChangeStock.cs
@@ -2,10 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Model;
+using Common;
+using System.Data.SqlClient;
 
 namespace IDAL
 {
+    /// <summary>
+    /// 调拨单
+    /// </summary>
     public interface IChangeStock
     {
+        /// <summary>
+        /// 增加调拨单
+        /// </summary>
+        /// <param name="changeStock"></param>
+        /// <param name="conn"></param>
+        /// <returns></returns>
+        int InsertChangeStock(ChangeStockInfo changeStock,SqlConnection conn);
+        /// <summary>
+        /// 更新调拨单
+        /// </summary>
+        /// <param name="category"></param>
+        /// <returns></returns>
+        int UpdateChangeStock(ChangeStockInfo changeStock, SqlConnection conn);
+        /// <summary>
+        /// 根据分类id删除调拨单
+        /// </summary>
+        /// <param name="category"></param>
+        /// <returns></returns>
+        int DeleteChangeStock(Guid changeStockId, SqlConnection conn);
+        /// <summary>
+        /// 根据条件获取调拨单
+        /// </summary>
+        /// <param name="conditon"></param>
+        /// <returns></returns>
+        IList<ChangeStockInfo> GetChangeStock(SearchCondition conditon, SqlConnection conn);
+        /// <summary>
+        /// 获取满足条件的调拨单数量
+        /// </summary>
+        /// <param name="conditon"></param>
+        /// <returns></returns>
+        int GetChangeStockCount(SearchCondition conditon, SqlConnection conn);
+        /// <summary>
+        /// 获取指定页的调拨单
+        /// </summary>
+        /// <param name="conditon"></param>
+        /// <param name="page">页数</param>
+        /// <param name="pagesize">每页数量</param>
+        /// <param name="conn"></param>
+        /// <returns></returns>
+        IList<ChangeStockInfo> GetPageChangeStock(SearchCondition conditon, int page, int pagesize, SqlConnection conn);
     }
 }

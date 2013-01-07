c172e3ed76120bd7c8c9679a906345c1789264a9
songhairui1110@gmail.com <songhairui1110@gmail.com@47e0d3c2-fcb2-9a0f-c3f3-96d527b66d0c>
2013-01-07 03:19:25 +0000

    delete some file and add productinfo.cs

--- trunk/shop/Model/ProductInfo.cs
+++ trunk/shop/Model/ProductInfo.cs
@@ -5,19 +5,16 @@ using System.Text;
 
 namespace Model
 {
-    public class Product:CommonInfo
+    public class ProductInfo:CommonInfo
     {
-        public int  id{get;set;}
+        public Guid  id{get;set;}
         public string ProductNO{get;set;}
         public string ProductName{get;set;}
-        public int CategoryID{get;set;}
-        public int TypeID{get;set;}
+        public Guid CategoryID{get;set;}
         public string ProductSKU{get;set;}
         public string ProductDetail{get;set;}
-        public int UnitID{get;set;}
         public decimal Price{get;set;}
         public decimal SailPrice{get;set;}
-        public int ColorID{get;set;}
         public string Place{get;set;}
         public DateTime SaveTime { get; set; }
         public string Define1{get;set;}
